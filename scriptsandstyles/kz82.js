07c85a9913c15f6a9c15811bdb8f04386790a1a4
Laceix <sebbyjeb@gmail.com>
2024-12-19 18:56:03 -0500

    koala site update 1.2

--- scriptsandstyles/kz82.js
+++ scriptsandstyles/kz82.js
@@ -6,9 +6,11 @@ function addFunc() {
   var total = num1 + num2;
   awnsr = window.prompt("what is " + num1 + " + " + num2);
   if (awnsr == total) {
-    document.getElementById("p1").innerHTML = "CORECT!!!";
+    document.getElementById("p1").innerHTML =
+      "CORECT!!! the awnser is " + total;
   } else {
-    document.getElementById("p1").innerHTML = "INCORECT!!!";
+    document.getElementById("p1").innerHTML =
+      "INCORECT!!! the awnser is " + total;
   }
 }
 function subFunc() {
@@ -18,9 +20,11 @@ function subFunc() {
   var total = num1 - num2;
   awnsr = window.prompt("what is " + num1 + " - " + num2);
   if (awnsr == total) {
-    document.getElementById("p1").innerHTML = "CORECT!!!";
+    document.getElementById("p1").innerHTML =
+      "CORECT!!! the awnser is " + total;
   } else {
-    document.getElementById("p1").innerHTML = "INCORECT!!!";
+    document.getElementById("p1").innerHTML =
+      "INCORECT!!! the awnser is " + total;
   }
 }
 function mulFunc() {
@@ -30,21 +34,28 @@ function mulFunc() {
   var total = num1 * num2;
   awnsr = window.prompt("what is " + num1 + " X " + num2);
   if (awnsr == total) {
-    document.getElementById("p1").innerHTML = "CORECT!!!";
+    document.getElementById("p1").innerHTML =
+      "CORECT!!! the awnser is " + total;
   } else {
-    document.getElementById("p1").innerHTML = "INCORECT!!!";
+    document.getElementById("p1").innerHTML =
+      "INCORECT!!! the awnser is " + total;
   }
 }
 function divFunc() {
   var num1 = Math.floor(Math.random() * 100);
-  var num2 = Math.floor(Math.random() * 10);
+  var num2 = Math.floor(Math.random() * 10, 2);
   let awnsr;
   var total = Math.floor(num1 / num2);
   awnsr = window.prompt("what is " + num1 + " / " + num2);
   if (awnsr == total) {
-    document.getElementById("p1").innerHTML = "CORECT!!!";
+    document.getElementById("p1").innerHTML =
+      "CORECT!!! the awnser is " + total;
+  } else if (total == "Infinity" && awnsr == 0) {
+    document.getElementById("p1").innerHTML =
+      "CORECT!!! the awnser is 0: well techicly this question is imposable but idk i'ma lazy dev :3";
   } else {
-    document.getElementById("p1").innerHTML = "INCORECT!!!";
+    document.getElementById("p1").innerHTML =
+      "INCORECT!!! the awnser is " + total;
   }
 }
 //math functions//
